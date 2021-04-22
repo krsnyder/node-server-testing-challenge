869ce8c4907e405a699b9bca64ad0884aaa87015
krsnyder <kirkrobertsnyder@gmail.com>
2021-04-22 14:38:30 -0400

    wip

---
+++ api/teams/teams-model.js
@@ -0,0 +1,21 @@
+const db = require('../../data/db-config');
+
+function getTeams() {
+  return null;
+}
+function getTeamById() {
+  return null;
+}
+function addTeam() {
+  return null;
+}
+function removeTeam() {
+  return null;
+}
+
+module.exports = {
+  getTeams,
+  getTeamById,
+  addTeam,
+  removeTeam,
+};
---
+++ api/teams/teams-model.test.js
@@ -0,0 +1,25 @@
+const Teams = require('./teams-model');
+const db = require('../../data/db-config');
+
+describe('Team model', () => {
+  describe('getTeams', () => {
+    it.todo('Todo test', () => {
+      expect(true).toBe(true);
+    });
+  });
+  describe('  getTeamById', () => {
+    it.todo('Todo test', () => {
+
+    });
+  });
+  describe('  addTeam', () => {
+    it.todo('Todo test', () => {
+
+    });
+  });
+  describe('  removeTeam', () => {
+    it.todo('Todo test', () => {
+
+    });
+  });
+});
