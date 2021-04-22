c9b4a570200ff38a1d14ec2e2f1bcf09ef2cf893
krsnyder <kirkrobertsnyder@gmail.com>
2021-04-22 12:40:58 -0400

    built index and server files, renamed resources

--- api/server.js
+++ api/server.js
@@ -1,5 +1,44 @@
+// declare instance of express
+// create server and invoke Express
+// Import any middleware
+// Teach express to parse JSON
+// Build error handling
+// Make calls to routers
+// Export module
+
 const Express = require('express');
 
+const Teams = require('./teams/teams-model');
+
 const server = Express();
+
+server.use(Express.json());
+
+// Place all requests
+
+server.get('/teams', (req, res) => {
+  res.end();
+});
+
+server.get('/teams/:id', (req, res) => {
+  res.end();
+});
+
+server.post('/teams', (req, res) => {
+  res.end();
+});
+
+server.delete('/teams', (req, res) => {
+  res.end();
+});
+
+// eslint-disable-next-line no-unused-vars
+server.use('*', (err, req, res, next) => {
+  res.status(500).json({
+    customMessage: 'Shit is broken',
+    message: err.message,
+    stack: err.stack,
+  });
+});
 
 module.exports = server;
